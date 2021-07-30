af40712746df9b20ae874d3b04ad13f1cd30fc4b
Bipul-Harsh <bipulharsh123@gmail.com>
2021-07-30 11:03:32 +0530

    The login page bg problem sorted

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,4 +1,3 @@
-import { start } from "@popperjs/core";
 import React, {useState, useEffect} from "react";
 import bgImage1 from "../assets/background1.jpg";
 import bgImage2 from "../assets/background2.jpg";
@@ -6,7 +5,7 @@ import bgImage3 from "../assets/background3.jpg";
 import bgImage4 from "../assets/background4.jpg";
 
 function Login(){
-    let [count, setCount] = useState(0);
+    let [counter, setCount] = useState(0);
     const bgImages = [
         bgImage1,
         bgImage2,
@@ -14,17 +13,18 @@ function Login(){
         bgImage4
     ];
 
-    function startTimer(){
-        setInterval(()=>{
-            setCount(count+1);
-            console.log('startTimer', count);
+    useEffect(()=>{
+        const timer = setInterval(()=>{
+            setCount(counter+1);
+            console.log('startTimer', counter);
         }, 5000);
-    }
 
-    startTimer();
+        return ()=>clearInterval(timer);
+    })
 
+    
     return(
-        <div id="login-window" className="container-fluid d-flex justify-content-center align-items-center" style={{backgroundImage:`url(${bgImages[count%4]}`}}>
+        <div id="login-window" className="container-fluid d-flex justify-content-center align-items-center" style={{backgroundImage:`url(${bgImages[counter%4]}`}}>
             <div classNameid="login-box" className="glassy-dark-bg border border-3 p-3 rounded-custom w-25">
                 <h2 className="logo-font text-center my-5">Cashie</h2>
                 <form action="" className="my-3">
